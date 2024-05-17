2f4ad8d03fe54d6637c021f5a0d2cbc56755f66d
Ekkiddle <ekkiddle@gmail.com>
2024-05-16 23:10:00 -0600

    styling

--- bankDice/screens/Players.js
+++ bankDice/screens/Players.js
@@ -7,20 +7,22 @@ const Player = ({name, onPress}) => (
         flexDirection:'row',
         width:'100%',
         height: 50,
-        borderWidth: 1,
+        borderRadius: 15,
         paddingHorizontal: 5,
         justifyContent:'space-between',
         alignItems:'center',
-        marginVertical:5,
+        marginVertical:8,
+        backgroundColor:'#274A17',
     }}>
         <Text style={{
         fontSize: 16,
-        margin: 10
+        margin: 10, color:'#F9C300', fontSize:24
         }}>{name}</Text>
         <TouchableOpacity
-                style={[styles.roundButton, {width: 25, height: 25, marginHorizontal:5}]}
+                style={{width: 45, height: 35, marginHorizontal:5, justifyContent:'center',
+                alignItems:'center', backgroundColor:'#396922', borderRadius:20}}
                 onPress={onPress}>
-                <AntDesign name="minus" size={20} color="white" />
+                <AntDesign name="minus" size={28} color="#F9C300" />
             </TouchableOpacity>
     </View>
 );
@@ -31,25 +33,37 @@ const Players = ({navigation}) => {
     const [players, setPlayers] = React.useState([]);
 
     return (
-        <View style={{ flex: 1, paddingHorizontal:10, paddingVertical:30 }}>
+        <View style={{ flex: 1, paddingHorizontal:10, paddingVertical:30, backgroundColor:'#203E11' }}>
             {/* User Input Section */}
+            <Text style={{ width:'100%', textAlign:'center', fontSize:36, color:'#F9C300'}}>Add Players</Text>
             <View style={{ flexDirection:'row', justifyContent:'flex-start', alignItems:'center', width:'100%', paddingRight: 6}}>
                 <TextInput 
                     style={{
-                        width:'90%',
-                        height: 30,
+                        width:'85%',
+                        height: 40,
                         marginRight:10,
                         marginVertical: 10,
                         borderWidth: 1,
                         paddingHorizontal: 10,
-                        fontSize: 16
+                        fontSize: 16,
+                        backgroundColor:'#274A17',
+                        borderColor:'#396922',
+                        borderRadius:20,
                     }}
+                    color={'#F9C300'}
                     onChangeText={onChangeText}
                     value={text}
                     placeholder='Player name'
+                    placeholderTextColor={'#396922'}
                 />
                 <TouchableOpacity
-                    style={styles.roundButton}
+                    style={{borderRadius:50,
+                        height: 40,
+                        width: 40,
+                        justifyContent:'center',
+                        alignItems:'center',
+                        backgroundColor: '#30571E',
+                        marginLeft:'auto'}}
                     onPress={() => {
                         if(text.trim()){
                             setPlayers([...players, { name: text.trim() , points: 0, banked: false }]);
@@ -57,30 +71,26 @@ const Players = ({navigation}) => {
                         };
                     }}
                     >
-                    <AntDesign name="plus" size={24} color="white" />
+                    <AntDesign name="plus" size={24} color="#F9C300" />
                 </TouchableOpacity>
             </View>
-
-            <Text> Players </Text>
+            <View style={{backgroundColor:'#AE8800', height:5, width:'110%', marginVertical:10, marginLeft:-15}}></View>
+            <View style={{backgroundColor:'#AE8800', height:2, width:'110%', marginBottom:15, marginLeft:-15}}></View>
             <FlatList
                 data={players}
                 renderItem={({item, index}) => <Player name={item.name} 
                 onPress={()=>{setPlayers(players.slice(0,index).concat(players.slice(index+1, players.length)))}} />}
             />
-            <Button title="start" disabled={players.length<2} onPress={() => navigation.navigate("Game", {list: players})}/>
+            <TouchableOpacity style={{ backgroundColor:players.length<2?'#274A17':'#F9C300', width:'100%', justifyContent:'center', alignItems:'center',
+                    borderRadius:50, height:50, borderWidth:1, borderColor:players.length<2?'#274A17':'#FFD230', marginVertical:5 }}
+                    onPress={() => navigation.navigate("Game", {list: players})} disabled={players.length<2}
+                >
+                    <Text style={{ fontSize:26, color:'#203E11'}}>
+                        Start
+                    </Text>
+                </TouchableOpacity>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    roundButton: {
-        borderRadius:50,
-        height: 30,
-        width: 30,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor: '#147EFB'
-    },
-  });
 
 export default Players;
--- bankDice/screens/Start.js
+++ bankDice/screens/Start.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 // Start page that will include options for starting a new game, 
 // spectating an exisiting game, or (future)
 // participating in a virtual dice/online game. (future)
 const Start = ({navigation}) => {
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text> Bank </Text>
-            <Button 
-                title="Play" 
-                onPress={() => navigation.navigate("Players")} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#203E11', padding:20 }}>
+            <Text style={{ fontSize:100, color:'#F9C300'}}> BANK </Text>
+            <TouchableOpacity style={{ backgroundColor:'#F9C300', width:'100%', justifyContent:'center', alignItems:'center',
+                    borderRadius:50, height:50, borderWidth:1, borderColor:'#FFD230', marginVertical:5 }}
+                    onPress={() => navigation.navigate("Players")}
+                >
+                    <Text style={{ fontSize:26, color:'#203E11'}}>
+                        Start
+                    </Text>
+                </TouchableOpacity>
         </View>
     );
 };
